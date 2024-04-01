e1606f3e8bbc917a64dd4b437f92c4468308df76
Erin O'Rourke <erinorourke17@gmail.com>
2024-04-01 13:32:02 -0500

    add tests, remove old pipelines

---
+++ test_main.py
@@ -0,0 +1,22 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_with_valid_input():
+    response = client.get("/test")
+    data = response.json()
+
+    assert response.status_code == status.HTTP_200_OK
+    assert "message" in data and data["message"] == "Hello World"
+
+
+def test_with_invalid_input():
+    response = client.post("/test")
+    data = response.json()
+
+    assert response.status_code != status.HTTP_200_OK
+    assert "Method Not Allowed" in data["detail"]
