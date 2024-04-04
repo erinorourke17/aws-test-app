dcc5fcbb37c9360a27be0b91f884234faf79eaae
Erin O'Rourke <erinorourke17@gmail.com>
2024-04-04 16:23:05 -0500

    make tests less brittle

--- test_main.py
+++ test_main.py
@@ -11,7 +11,7 @@ def test_with_valid_input():
     data = response.json()
 
     assert response.status_code == status.HTTP_200_OK
-    assert "message" in data and data["message"] == "Hello World"
+    assert "message" in data
 
 
 def test_with_invalid_input():
